9d65a064af3007c1337740122eb6950e08fd6b46
traineeonezabnix <traineeonezabnix@gmail.com>
2026-06-30 15:47:28 +0530

    feat: configure Vercel routing and enable Flutter path URL strategy for web deployment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:fitness_webapp/frontend/front.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const FitPilotApp());
 }
+
